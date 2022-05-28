0aba4b807234e927be6d34f84a8c24fde7262121
Ilya G. Mirin <ilya.mirin@gmail.com>
2022-05-28 21:51:29 +0300

    init

---
+++ platonov.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, url_for
+from os import listdir
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return render_template('main.html')
+
+
+@app.route("/pictures_urls")
+def pictures_urls():
+    result = {'pictures': list()}
+    for pict in listdir('static\\pictures\\'):
+        result['pictures'].append(url_for('static', filename='pictures/'+pict))
+    return result
+
